9ad6911fddb18d8d9d4a811b64c4fbf133e61db3
Wallisson Martins <wallissonmartins37@gmail.com>
2023-07-08 14:41:55 -0300

    retorno de lista

--- src/main/java/com/apisimple/demo/controller/ProductController.java
+++ src/main/java/com/apisimple/demo/controller/ProductController.java
@@ -1,20 +1,29 @@
 package com.apisimple.demo.controller;
 
 import com.apisimple.demo.entities.Department;
+import com.apisimple.demo.entities.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
     @GetMapping()
-    public Department getObjects() {
-        Department d1 = new Department();
-        d1.setId(1L);
-        d1.setName("Tech");
+    public List<Product> getObjects() {
+        Department d1 = new Department(1L, "Tech");
+        Department d2 = new Department(2L, "Pet");
 
-        return d1;
+        Product p1 = new Product(1L, "Mackbook Pro", 4000.0, d1);
+        Product p2 = new Product(1L, "Mackbook Pro", 4000.0, d1);
+        Product p3 = new Product(1L, "Mackbook Pro", 4000.0, d2);
+
+        List<Product> list = Arrays.asList(p1, p2, p3);
+
+        return list;
     }
 }
--- src/main/java/com/apisimple/demo/entities/Department.java
+++ src/main/java/com/apisimple/demo/entities/Department.java
@@ -4,6 +4,15 @@ public class Department {
     private Long id;
     private String name;
 
+    public Department() {
+    }
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
     public Long getId() {
         return id;
     }
